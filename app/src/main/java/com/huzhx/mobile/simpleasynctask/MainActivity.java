c9f59cf47fd18ead98874c4449f0f8ac9e2d8c47
Zhaoxian Hu <huzhx@uci.edu>
2019-09-14 14:38:54 -0700

    init commit

---
+++ app/src/main/java/com/huzhx/mobile/simpleasynctask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huzhx.mobile.simpleasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
